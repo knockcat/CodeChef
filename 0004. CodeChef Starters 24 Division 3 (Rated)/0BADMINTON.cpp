a8ea49bcb69dda0d138def3a29b9a814e0423791
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-03 06:23:34 +0530

    BADMINTON

---
+++ 0004. CodeChef Starters 24 Division 3 (Rated)/0BADMINTON.cpp
@@ -0,0 +1,26 @@
+// https://www.codechef.com/START24C/problems/BADMINTON
+
+// Badminton Serves
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int p;
+        cin >> p;
+
+        cout << (p / 2) + 1 << endl;
+    }
+    return 0;
+}
+
+// Solution Link
+
+// https://www.codechef.com/viewsolution/57509309
