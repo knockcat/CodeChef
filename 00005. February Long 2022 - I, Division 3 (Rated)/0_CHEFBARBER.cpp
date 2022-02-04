bbb80be6287bf08a2600d32ec431d09634b879b1
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-04 21:27:01 +0530

    CHEFBARBER

---
+++ 00005. February Long 2022 - I, Division 3 (Rated)/0_CHEFBARBER.cpp
@@ -0,0 +1,26 @@
+// https://www.codechef.com/FEB221C/problems/CHEFBARBER
+
+// Chef and the Hair Salon
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // KNOCKCAT
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+
+        cout << m * n << endl;
+    }
+    return 0;
+}
+
+// Solution Link
+
+// https://www.codechef.com/viewsolution/57758707
