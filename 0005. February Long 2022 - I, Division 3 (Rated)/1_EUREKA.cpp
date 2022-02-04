b340a31181c3f2ea38654c966dbd9d5557870f21
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-04 21:31:42 +0530

    Long Challenge

---
+++ 0005. February Long 2022 - I, Division 3 (Rated)/1_EUREKA.cpp
@@ -0,0 +1,28 @@
+// https://www.codechef.com/FEB221C/problems/EUREKA
+
+// N Queens Puzzle Solved !
+
+#include <bits/stdc++.h>
+// #include <iostream>
+// #include<math.h>
+using namespace std;
+
+int main()
+{
+    // KNOCKCAT
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        cout << round(pow(0.143 * n, n)) << endl;
+    }
+    return 0;
+}
+
+// Solution Link
+
+// https://www.codechef.com/viewsolution/57759757
