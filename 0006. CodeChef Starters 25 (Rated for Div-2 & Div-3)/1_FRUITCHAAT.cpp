8df024898331a648b851be8b4e66abdeb18a8488
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-10 09:30:04 +0530

    FRUITCHAAT

---
+++ 0006. CodeChef Starters 25 (Rated for Div-2 & Div-3)/1_FRUITCHAAT.cpp
@@ -0,0 +1,30 @@
+// https://www.codechef.com/problems/FRUITCHAAT
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// KNOCKCAT
+	
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    int x, y;
+	    cin >> x >> y;
+	    
+	   if(x/2 <= y)
+	        cout<<x/2<<endl;
+	   else if(x >= y*2)
+	        cout<<y<<endl;
+	        
+	    
+	    
+	}
+	return 0;
+}
+
+// Solution Link
+
+// https://www.codechef.com/viewsolution/58152866
