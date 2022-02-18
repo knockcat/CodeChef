29ed6f7b5a7d457280063fd95a251de9d93fb4f7
VICKY <vishujoshi21nov2000@gmail.com>
2022-02-18 20:45:00 +0530

    NOTEBOOK

---
+++ 0008. CodeChef Starters 26 Division 3 (Rated)/1_NOTEBOOK.cpp
@@ -0,0 +1,26 @@
+// https://www.codechef.com/START26C/problems/NOTEBOOK
+
+// Count the Notebooks
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// KNOCKCAT \\
+	
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    
+	    cout << n * 10 << endl;
+	}
+	return 0;
+}
+
+// Solution Link
+
+// https://www.codechef.com/viewsolution/58469684
