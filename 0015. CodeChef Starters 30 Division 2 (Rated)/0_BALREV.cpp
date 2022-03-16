204bb5cd58ebeb3f707588fd71c9615c3a6b9aea
VICKY <vishujoshi21nov2000@gmail.com>
2022-03-16 20:49:50 +0530

    BALREV

---
+++ 0015. CodeChef Starters 30 Division 2 (Rated)/0_BALREV.cpp
@@ -0,0 +1,34 @@
+// https://www.codechef.com/START30B/problems/BALREV
+
+// Balanced Reversals
+
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+
+int main()
+{
+    // KNOCKCAT
+    
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n ;
+        string s;
+        cin >> s;
+
+        sort(s.begin(),s.end());
+
+        cout<<s<<endl;
+    }
+
+    return 0;
+    
+}
+
+// Solution Link
+
+// https://www.codechef.com/viewsolution/60561849
