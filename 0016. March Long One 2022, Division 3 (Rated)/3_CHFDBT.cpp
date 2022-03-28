59337b35df73c787ab59d6077e0c32227d27d75e
VICKY <vishujoshi21nov2000@gmail.com>
2022-03-28 20:50:20 +0530

    CHFDBT

---
+++ 0016. March Long One 2022, Division 3 (Rated)/3_CHFDBT.cpp
@@ -0,0 +1,29 @@
+// https://www.codechef.com/MARCH221C/problems/CHFDBT
+
+// Akash and Function
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+	// KNOCKCAT
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    
+	    cout<<(n+1)/2<<endl;
+	}
+	
+	return 0;
+}
+
+
+// Solution Link
+
+// https://www.codechef.com/MARCH221C/status/CHFDBT,knockcat_1
