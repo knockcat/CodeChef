3c2e9d4d308c337cf5a9365edeba625df53d8013
VICKY <vishujoshi21nov2000@gmail.com>
2022-03-28 21:03:18 +0530

    GROUPASSIGN

---
+++ 0018. CodeChef Starters 31 Division 3 (Rated)/1_GROUPASSGN.cpp
@@ -0,0 +1,28 @@
+// https://www.codechef.com/START31C/problems/GROUPASSGN
+
+// Group Assignment
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	// KNOCKCAT
+	
+	int t;
+	cin >> t;
+	
+	while(t--)
+	{
+	    int n,x;
+	    cin >> n  >> x;
+	    
+	    cout << n*2 - x + 1<<endl;
+	}
+	return 0;
+}
+
+
+// Solution Link
+
+// https://www.codechef.com/viewsolution/61062275
+
