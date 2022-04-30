d3a5f8c310bada9641e07f7b458928b7138f40fb
VICKY <vishujoshi21nov2000@gmail.com>
2022-04-30 19:50:08 +0530

    Email Reminders

---
+++ 0020. April Cook-Off 2022 Division 4/1_EMAILREM.cpp
@@ -0,0 +1,29 @@
+// https://www.codechef.com/COOK140D/problems/EMAILREM
+
+// Email Reminders
+
+#include <iostream>
+using namespace std;
+
+void solve();
+
+int main()
+{
+    // KNOCKCAT
+
+    solve();
+
+    return 0;
+}
+
+void solve()
+{
+    int num, user;
+    cin >> num >> user;
+    int ans = num - user;
+    cout << ans << endl;
+}
+
+// Solution Link
+
+// https://www.codechef.com/viewsolution/61880935
