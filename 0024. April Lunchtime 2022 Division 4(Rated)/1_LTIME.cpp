6f302a285eb2c486464bf249896b163890137ddf
VICKY <vishujoshi21nov2000@gmail.com>
2022-05-02 08:43:56 +0530

    LunchTime

---
+++ 0024. April Lunchtime 2022 Division 4(Rated)/1_LTIME.cpp
@@ -0,0 +1,29 @@
+// https://www.codechef.com/LTIME107D/problems/LTIME
+
+// Lunchtime
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // KNOCKCAT
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int x;
+        cin >> x;
+        if (x >= 1 && x <= 4)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
+
+// Soltion Link
+
+// https://www.codechef.com/viewsolution/63062813
